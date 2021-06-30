37878433135ffcbcb8125d6013fb272a2a69dc03
booksssss <86739545+booksssss@users.noreply.github.com>
2021-06-30 10:54:26 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+
+function setup() {
+  createCanvas(400,400);
+   box = createSprite (30,30,10,10);
+}
+
+function draw() 
+{
+  background(30);
+  if (keyDown(UP_ARROW)) {
+    box.y = box.y-5
+  }
+  if (keyDown(DOWN_ARROW)) {
+    box.y = box.y+5
+  }
+  if (keyDown(LEFT_ARROW)) {
+    box.x = box.x-5
+  }
+  if (keyDown(RIGHT_ARROW)) {
+    box.x = box.x+5
+  }
+drawSprites();
+}
+
+
+
+
